fc30c276b2f4d5920c0b2d0508d76ff7f7034308
Evan <evanpeterson17@gmail.com>
2017-03-22 19:34:37 -0600

    Added typeahead functionality to searchbar in Products page, using 3rd party typeahead.js

--- SportsStore.WebUI/Models/ProductsListViewModel.cs
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -11,5 +11,15 @@ namespace SportsStore.WebUI.Models
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
         public string CurrentCategory { get; set; }
+        public string JsonProducts {
+            get
+            {
+                var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+                var json = serializer.Serialize(Products.Select(p => p.Name));
+                var decodedJson = HttpUtility.HtmlDecode(json);
+
+                return json;
+            }
+        }
     }
 }
